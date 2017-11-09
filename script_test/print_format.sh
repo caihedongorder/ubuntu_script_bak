1768767b907dc5ece1fc8c967a26c43242351ade
caihedongorder <caihedongorder@126.com>
2017-11-09 02:58:17 -0800

    =add some exercise

---
+++ script_test/print_format.sh
@@ -0,0 +1,9 @@
+#/usr/bin/sh
+
+echo "echo normal format"
+echo -e "\033[1mEcho bold format\033[0m"
+echo -e "\033[7mEcho Selected Text format\033[0m"
+
+echo -e "\033[33;5mEcho blinking Text format\033[0m"
+echo -e "\033[33;7mEcho blinking Text format\033[0m"
+echo -e "\033[33;5;7mEcho blinking Text format\033[0m"
